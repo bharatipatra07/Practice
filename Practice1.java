a83590354d0aabc437eeaaf0c38286d6b2c03f3e
E Bharati Patra <bharati.patra@bridgelabz.com>
2026-04-21 10:07:56 +0530

    UC2

---
+++ Practice1.java
@@ -0,0 +1,16 @@
+public class Practice1{
+    public static void main(String[] args){
+        int[] vals={29,89,10,90,100};
+        int max=Integer.MIN_VALUE;
+        int secondMax=Integer.MIN_VALUE;
+        for(int val:vals){
+             if(val>max){
+                secondMax=max;
+                max=val;
+                }else if(val>secondMax && val!=max){
+                    secondMax=val;
+                }
+        }
+        System.out.println(secondMax);
+    }
+}
